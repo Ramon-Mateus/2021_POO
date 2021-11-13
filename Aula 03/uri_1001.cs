efa91f36a6095500c92e9f3f1b83d930c18b774c
Ramon-Mateus <60753390+Ramon-Mateus@users.noreply.github.com>
2021-11-13 20:34:24 -0300

    Create uri_1001.cs

---
+++ Aula 03/uri_1001.cs
@@ -0,0 +1,11 @@
+using System;
+
+
+ class Program{
+   static void Main(string[] args){
+     int a = int.Parse(Console.ReadLine());
+     int b = int.Parse(Console.ReadLine());
+     int c = a + b;
+     Console.WriteLine($"X = {c}");
+   }
+ }
