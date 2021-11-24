055e98a2c43146c071f156711094de55f1949e77
Ramon Mateus <60753390+Ramon-Mateus@users.noreply.github.com>
2021-11-24 05:01:14 +0000

    Enviando o EX1044.cs

---
+++ Lista 05/Ex1044.cs
@@ -0,0 +1,16 @@
+using System;
+
+class mainClass {
+  public static void Main (String[] args) {
+    string x = Console.ReadLine();
+    string[] v = x.Split(' ');
+    int x1 = int.Parse(v[0]);
+    int x2 = int.Parse(v[1]);
+    
+    if (x1 % x2 == 0 || x2 % x1 == 0) {
+      Console.WriteLine("Sao Multiplos");
+    } else {
+      Console.WriteLine("Nao sao Multiplos");
+    }
+  }
+}
