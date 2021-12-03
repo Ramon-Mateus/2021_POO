e147e21c912dc2c4de84ae476eaa5cf00382a2b3
Ramon de Oliveira Araújo Mateus <60753390+Ramon-Mateus@users.noreply.github.com>
2021-12-03 20:37:38 +0000

    att Ex05.cs

--- Lista 07/Ex05.cs
+++ Lista 07/Ex05.cs
@@ -8,6 +8,14 @@ class mainClass {
     Console.WriteLine(r);
   }
   public static string FormatarNome(string nome) {
-    
+    nome = nome.ToLower();
+    string[] v = nome.Split(' ');
+    string res = "";
+    for (int i = 0; i < v.Length; i++) {
+      char pl = char.ToUpper(v[i][0]);
+      string names =  v[i].Substring(1);
+      res += pl + names + " ";
+    }
+    return res;
   }
 }
